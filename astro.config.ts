3158e410b0568af99650778d805ac63438ab9bc1
bash-cloud <din.bashir00@gmail.com>
2024-10-27 15:04:27 +0300

    changes

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: 'portfolio',
   integrations: [
     tailwind({
       applyBaseStyles: false,
