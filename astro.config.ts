5aaf21731b107c8deaaa894d57d6c4f9c01d1c0c
bash-cloud <din.bashir00@gmail.com>
2024-10-27 15:30:10 +0300

    urls fix

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: 'portfolio',
+  base: '/portfolio /',
   integrations: [
     tailwind({
       applyBaseStyles: false,
